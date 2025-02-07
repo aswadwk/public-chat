1cdfed23754f7b8fc1a430f9645f9427efa98c7f
aswadwk <hajaraswadkom@gmail.com>
2025-02-07 10:18:24 +0700

    refactor: remove unused Separator component from DefaultLayout and fix import case for DefaultLayout in Dashboard

--- resources/js/Components/Layout/DefaultLayout.tsx
+++ resources/js/Components/Layout/DefaultLayout.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { SidebarInset, SidebarProvider, SidebarTrigger } from "../ui/sidebar";
 import AppSidebar from "@/Shared/Sidebar";
-import { Separator } from "../ui/separator";
 import { BreadcrumbWithCustomSeparator } from "../Breadcrumb";
 
 const DefaultLayout = ({ children }: { children: React.ReactNode }) => {
@@ -12,7 +11,6 @@ const DefaultLayout = ({ children }: { children: React.ReactNode }) => {
         <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
           <div className="flex items-center gap-2 px-4">
             <SidebarTrigger />
-            <Separator orientation="vertical" className="h-4 mr-2" />
 
             <BreadcrumbWithCustomSeparator />
           </div>
--- resources/js/Pages/Dashboard/Dashboard.tsx
+++ resources/js/Pages/Dashboard/Dashboard.tsx
@@ -1,4 +1,4 @@
-import DefaultLayout from "@/components/Layout/DefaultLayout";
+import DefaultLayout from "@/Components/Layout/DefaultLayout";
 
 export const Dashboard = () => {
   return <DefaultLayout>Dashboard</DefaultLayout>;
